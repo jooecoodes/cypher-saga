2ccc920455339bd3f7469228e72ce2c622895215
jooecoodes <jefeljoevillacorta@gmail.com>
2025-04-02 20:38:44 +0800

    Main Menu UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
 // Encryptions
 #include "../include/core/Core.hpp"
 
+#include "../include/ui/UI.hpp"
+
 
 #include <iostream>
 
 int main() {
+
+    UI::loadingBar("Loading", 1000, 90);
+    UI::breakDisplay(50);
+    UI::logoDisplay();
+    UI::breakDisplay(50);
+    UI::introDisplay();
+    UI::breakDisplay(50);
+
+
+    // bool sentinel = true;
+    // while (sentinel) {
+        
+    // }
     // Initialize a 3×3 matrix with example values
-    Matrix<double> mat({{
-        {5.0, 8.0, 12.0},
-        {1.0, 2.0, 4.0},
-        {10.0, 11.0, 12.0}
-    }});
-    std::string input = "Hello, World!";
-    std::vector<Vector<double>> encryptResult = Core::encryptedVectors(input, mat);
-    std::vector<Vector<double>> decryptResult = Core::decryptedVectors(encryptResult, mat);
+    // Matrix<double> mat({{
+    //     {5.0, 8.0, 12.0},
+    //     {1.0, 2.0, 4.0},
+    //     {10.0, 11.0, 12.0}
+    // }});
+    // std::string input = "Hello, World!";
+    // std::vector<Vector<double>> encryptResult = Core::encryptedVectors(input, mat);
+    // std::vector<Vector<double>> decryptResult = Core::decryptedVectors(encryptResult, mat);
 
     // std::cout << "This is for encryptResult" << std::endl;
     // for (const auto &vec : encryptResult) {
@@ -31,8 +46,8 @@ int main() {
     //     std::cout << std::endl;
     // }
 
-    std::string resultFromVectors = processVectorsToString<double>(decryptResult);
-    std::cout << "This is the result for decryption: " << resultFromVectors << std::endl;
+    // std::string resultFromVectors = processVectorsToString<double>(decryptResult);
+    // std::cout << "This is the result for decryption: " << resultFromVectors << std::endl;
 
     return 0;
 }
